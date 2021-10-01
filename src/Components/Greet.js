f2331da9e5427a70137214a792304c345b1bdfa7
Amisha Prajapati <86883460+amisha123prajapati@users.noreply.github.com>
2021-10-01 11:32:55 +0530

    Add files via upload

---
+++ src/Components/Greet.js
@@ -0,0 +1,188 @@
+import React from 'react';
+
+function Greet (){
+    return <div>
+        <diV className="div">
+    <div className="head">
+            <div className="first-line">Order the food you love</div>
+            <input type="text" className="input" placeholder="Search Food"></input>
+            <div className="your-icon"></div>
+            <div className="search"></div>
+            <div className="full-rec">
+        <span className="rec1">All</span><span className="rec2">Categories</span>
+          </div>
+          <div className="big-rectangle">
+              <div className="para">Category A</div>
+              <div className="small-rectangle">
+                  <div className="item1">Item Name 1</div>
+                  <div className="price1">Rs. 450</div>
+                  <div className="sathi"><button className="p-btn">+</button>  4<button className="n-btn">-</button></div>
+                  
+                  <div className="small-rectangle" id="chan" >
+                  <div className="item1">Item Name 2</div>
+                  <div className="price1">Rs. 550</div>
+                  <div className="sathi"><button className="p-btn">+</button>  3<button className="n-btn">-</button></div>
+                </div>
+                
+                <div className="small-rectangle" id="change" >
+                  <div className="item1">Item Name 3</div>
+                  <div className="price1">Rs. 750</div>
+                  <div className="sathi"><button className="p-btn">Add</button></div>
+                 </div>
+            
+                 <div className="small-rectangle" id="change" >
+                  <div className="item1">Item Name 4</div>
+                  <div className="price1">Rs. 350</div>
+                  <div className="sathi"><button className="p-btn">+</button>  5<button className="n-btn">-</button></div>
+                </div>
+
+                <div className="small-rectangle" id="change" >
+                  <div className="item1">Item Name 5</div>
+                  <div className="price1">Rs. 350</div>
+                  <div className="sathi"><button className="p-btn">Add</button></div>
+                 </div>
+
+                 <div className="small-rectangle" id="change" >
+                  <div className="item1">Item Name 6</div>
+                  <div className="price1">Rs. 600</div>
+                  <div className="sathi"><button className="p-btn">Add</button></div>
+                 </div>
+            
+                
+                 <div className="small-rectangle" id="change" >
+                  <div className="item1">Item Name 7</div>
+                  <div className="price1">Rs. 500</div>
+                  <div className="sathi"><button className="p-btn">Add</button></div>
+                 </div>
+                 <div className="cart"></div>
+                </div>
+            
+
+                </div>
+                </div>
+                  
+
+            <div className="second-page">
+            <div className="first-line">Order the food you love</div>
+            <input type="text" className="input" placeholder="Search Categories"></input>
+            <div className="your-icon"></div>
+            <div className="search"></div>
+            <div className="full-rec">
+            <span className="rec3">All</span><span className="rec4">Categories</span>
+            </div>
+
+
+                    <div className="wer">
+                      <div className="recter"><div className="circle" id="cir1"></div> Category A  </div>
+                      <div className="recter"><div className="circle" id="cir2"></div> Category B  </div>
+                      <div className="recter"><div className="circle" id="cir3" ></div> Category C </div>
+                      <div className="recter"><div className="circle" id="cir4" ></div> Category D </div>
+                      <div className="recter"><div className="circle" id="cir5" ></div> Category E </div>
+                      <div className="recter"><div className="circle" id="cir6" ></div> Category F </div>
+                      <div className="recter"><div className="circle" id="cir7"></div> Category G  </div>
+                      <div className="recter"><div className="circle" id="cir8"></div> Category H  </div>
+                      <div className="recter"><div className="circle" id="cir9" ></div> Category I </div>
+                      <div className="recter"><div className="circle" id="cir10"></div> Category J </div>
+                      <div className="recter"><div className="circle" id="cir11"></div> Category K </div>
+                      <div className="recter"><div className="circle" id="cir12"></div> Category L </div>
+                    </div>
+                  </div>
+
+
+                 <div className="cart-page">
+                  <div className="header"></div>
+                  <h1>Cart</h1>
+                  <div className="back-icon"></div>
+                  
+                  
+                  <div className="ch"  id="top-1">
+                  <div className="item1">Item Name 1</div>
+                  <div className="price1">Rs. 450</div>
+                  <div className="sathi"><button className="p-btn">+</button>  4<button className="n-btn">-</button></div>
+
+
+                  <div className="ch" id="chania" >
+                  <div className="item1">Item Name 2</div>
+                  <div className="price1">Rs. 550</div>
+                  <div className="sathi"><button className="p-btn">+</button>  3<button className="n-btn">-</button></div>
+                </div>
+
+                <div className="ch" id="change" >
+                  <div className="item1">Item Name 4</div>
+                  <div className="price1">Rs. 350</div>
+                  <div className="sathi"><button className="p-btn">+</button>  5<button className="n-btn">-</button></div>
+                </div>
+
+                <div className="ch" id="change" >
+                  <div className="item1">Item Name 11</div>
+                  <div className="price1">Rs. 275</div>
+                  <div className="sathi"><button className="p-btn">+</button>  5<button className="n-btn">-</button></div>
+                </div>
+
+
+                <div className="ch" id="change" >
+                  <div className="item1">Item Name 24</div>
+                  <div className="price1">Rs. 900</div>
+                  <div className="sathi"><button className="p-btn">+</button>  6<button className="n-btn">-</button></div>
+                </div>
+
+                <div className="ch" id="change" >
+                  <div className="item1">Item Name 26</div>
+                  <div className="price1">Rs. 100</div>
+                  <div className="sathi"><button className="p-btn">+</button>  7<button className="n-btn">-</button></div>
+                </div>
+                 
+
+
+                <div className="total-full"><button className="total">Total</button><span className="full-money">RS. 12675</span></div>
+
+                <div><button className="complete-order">Complete Order</button></div>
+
+
+                 </div>
+                 </div>
+                 </diV>
+                 </div>
+                
+        
+              
+              
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+              
+}
+
+export default Greet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
